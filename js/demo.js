971bff72f865b66b3772ebec39fa6e6ba5388028
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2021-07-14 21:50:23 -0700

    Initial commit of prototype tutorial page.

---
+++ js/demo.js
@@ -0,0 +1,17 @@
+anime({
+  targets: ".incoming",
+  loop: true,
+  keyframes: [{ translateX: 140 }, { translateX: 140 }],
+  duration: 2500,
+  easing: "easeInSine",
+});
+
+anime({
+  targets: ".outgoing",
+  loop: true,
+  keyframes: [
+    { translateX: 140, easing: "easeInSine" },
+    { translateX: 0, easing: "easeOutSine" },
+  ],
+  duration: 2500,
+});
